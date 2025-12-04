0d655d027a9059994957f9bace1254515b6a9a64
adedola12 <dolapo836@gmail.com>
2025-12-04 11:11:02 +0100

    about Layout

---
+++ client/src/components/About/BriefInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BriefInfo = () => {
+  return <div>BriefInfo</div>;
+};
+
+export default BriefInfo;
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import AboutHero from "../components/About/AboutHero";
+import BriefInfo from "../components/About/BriefInfo";
+import Journey from "../components/About/Journey";
+import Tools from "../components/About/Tools";
+import DesignProcess from "../components/Home/DesignProcess";
+import BuildSection from "../components/Home/BuildSection";
 
 const About = () => {
-  return <div className="text-white">About</div>;
+  return (
+    <div className="text-white bg-[#050505]">
+      <AboutHero />
+      <BriefInfo />
+      <Journey />
+      <Tools />
+      <DesignProcess />
+      <BuildSection />
+    </div>
+  );
 };
 
 export default About;
